01347a78463ed02400ed4f01865d743e50daf001
Leviinson <votosi197@gmail.com>
2024-02-11 12:37:32 +0100

    updated: caching enabled

--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     "userprofile.apps.UserprofileConfig",
     "cart.apps.CartConfig",
     "location.apps.LocationConfig",
-    "fillers.apps.FillersConfig"
+    "fillers.apps.FillersConfig",
     # "delivery_and_payment.apps.DeliveryAndPaymentConfig",
     # "reviews.apps.ReviewsConfig",
 ]
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
     "catalogue/templates/",
     "cart/templates/",
     "orders/templates/",
-    "fillers/templates/"
+    "fillers/templates/",
 ]
 
 MEDIA_URL = "/media/"
@@ -169,17 +169,17 @@ DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.dummy.DummyCache",
-        "LOCATION": BASE_DIR / "api_cache",
-    }
-}
 # CACHES = {
 #     "default": {
-#         "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+#         "BACKEND": "django.core.cache.backends.dummy.DummyCache",
 #         "LOCATION": BASE_DIR / "api_cache",
 #     }
 # }
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": BASE_DIR / "api_cache",
+    }
+}
 
 CACHE_MIDDLEWARE_SECONDS = 10
